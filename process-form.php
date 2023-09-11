10dde6eaaf54409a093d60534216277398774b82
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-09-11 18:03:26 -0400

    about me

--- process-form.php
+++ process-form.php
@@ -4,17 +4,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
     
-    // Replace with your email address
     $to = "ryan.howard1993@gmail.com";
     $subject = "Contact Form Submission from $name";
     $headers = "From: $email";
     
     if (mail($to, $subject, $message, $headers)) {
-        // Redirect back to the same page with a success parameter
         header("Location: index.html?success=1");
         exit;
     } else {
-        // Redirect back to the same page with an error parameter
         header("Location: index.html?error=1");
         exit;
     }
